c4d1cec82819c5b6b628223de3b9412cebe0aa6f
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-19 14:23:20 -0500

    update: init

--- init.sql
+++ init.sql
@@ -34,9 +34,5 @@ CREATE TABLE Followers (
     CHECK (Status IN (0, 1))
 );
 
-INSERT INTO Preferences (Description) VALUES ('Música'), ('Viajes'), ('Cine');
+INSERT INTO Preferences (Description) VALUES ('Musica'), ('Viajes'), ('Deportes'),('Cine'),('Teatro'),('Comedia');
 INSERT INTO Types (Description) VALUES ('Personal'), ('Empresa');
-
-CREATE USER 'ascd1'@'%' IDENTIFIED WITH mysql_native_password BY '1716983349';
-GRANT ALL PRIVILEGES ON *.* TO 'ascd1'@'%';
-FLUSH PRIVILEGES;
